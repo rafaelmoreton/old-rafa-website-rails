fc4f4c5f359bfda759341780a444fac0006d527d
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2023-06-20 10:05:26 -0300

    Add home. subdomain as an allowed host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Add home. subdomain as a host
+  config.hosts << "home.rafaelmoreton.com"
 end
